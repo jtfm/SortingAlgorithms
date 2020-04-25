82e2d230adbf8b24e3eb952d03faf3a501e11ffa
Jack Milner <jack.milner@bigupdata.co.uk>
2020-04-25 15:23:01 +0100

    Add project files.

---
+++ SortingAlgorithms/ListGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortingAlgorithms
+{
+    public static class ListGenerator
+    {
+        public static List<int> GenerateListOfIntegers(int length, int min, int max)
+        {
+            var list = new List<int>();
+
+            for (var n = 0; n < length; n++)
+            {
+                list.Add(new Random().Next(min, max));
+            }
+
+            return list;
+        }
+    }
+}
---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = ListGenerator.GenerateListOfIntegers(100, 0, 100);
+            Console.WriteLine(list);
+        }
+    }
+}
